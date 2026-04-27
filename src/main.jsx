988e883b54c1c892b11ad2c334f13c184332e7fb
almeidatech <eemerson.almeida@gmail.com>
2026-04-27 11:59:03 -0300

    Wire internal links + add mobile responsive overrides

--- src/main.jsx
+++ src/main.jsx
@@ -4,6 +4,7 @@ import { BrowserRouter } from 'react-router-dom';
 import App from './App.jsx';
 import './styles/tokens.css';
 import './index.css';
+import './styles/responsive.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
